a4237214f1324c6dc2896df5cdfe342d65759346
Atwebuz <abdusattorov2004@mail.ru>
2023-10-07 23:28:46 +0500

    pathlib va destination download path yordamida fayllarni bitta popkaga yuklash va rasmni eng sifatli oxirgisini olish [-1] yordamida

--- handlers/users/docs_handlers.py
+++ handlers/users/docs_handlers.py
@@ -1,6 +1,11 @@
 import logging
 from loader import dp, bot
 from aiogram.types import ContentType, Message
+from pathlib import Path
+
+# download_path = Path().joinpath("downloads", "categories") ## downloads ichidagai categories popka yoli
+download_path = Path().joinpath("downloads") ## downloads path
+download_path.mkdir(parents=True, exist_ok=True)
 
 @dp.message_handler()
 async def text_handler(message: Message):
@@ -8,12 +13,17 @@ async def text_handler(message: Message):
 
 @dp.message_handler(content_types=ContentType.DOCUMENT)
 async def doc_handler(message: Message):
-    await message.document.download()
+    await message.document.download(destination=download_path)
     doc_id = message.document.file_id
     await message.reply("siz hujjat yubordingiz\n"f"file_id = {doc_id}")
 
 @dp.message_handler(content_types=ContentType.VIDEO)
 async def doc_handler(message: Message):
-    await message.video.download()
-    vid_id = message.video.vid_id
-    await message.reply("siz video yubordingiz\n"f"vid_id = {vid_id}")
+    await message.video.download(destination=download_path)
+    await message.reply("siz video yubordingiz\n"f"vid_id = {message.video.file_id}")
+
+
+@dp.message_handler(content_types=ContentType.PHOTO)
+async def doc_handler(message: Message):
+    await message.photo[-1].download(destination=download_path)
+    await message.reply("siz rasm yubordingiz\n"f"file_id = {message.photo[-1].file_id}")
