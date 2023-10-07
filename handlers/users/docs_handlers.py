3e828642492d2ed30abde3ae86a15e3aabbfcec0
Atwebuz <abdusattorov2004@mail.ru>
2023-10-07 23:34:06 +0500

    lubboy faylni yuborish

--- handlers/users/docs_handlers.py
+++ handlers/users/docs_handlers.py
@@ -18,12 +18,17 @@ async def doc_handler(message: Message):
     await message.reply("siz hujjat yubordingiz\n"f"file_id = {doc_id}")
 
 @dp.message_handler(content_types=ContentType.VIDEO)
-async def doc_handler(message: Message):
+async def videoc_handler(message: Message):
     await message.video.download(destination=download_path)
     await message.reply("siz video yubordingiz\n"f"vid_id = {message.video.file_id}")
 
 
 @dp.message_handler(content_types=ContentType.PHOTO)
-async def doc_handler(message: Message):
+async def photo_handler(message: Message):
     await message.photo[-1].download(destination=download_path)
     await message.reply("siz rasm yubordingiz\n"f"file_id = {message.photo[-1].file_id}")
+
+# lubboy faylni yuborish
+@dp.message_handler(content_types=ContentType.ANY)
+async def any_handler(message: Message):
+    await message.reply(f"file_id = {message.content_type} qabul qilindi")
