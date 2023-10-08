ec9bacf399d89688cc08b1e5900c2334c51c01c8
Atwebuz <abdusattorov2004@mail.ru>
2023-10-08 21:08:30 +0500

    the international bot was fixed

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,7 +2,7 @@ from . import help
 from . import start
 from . import telegraph
 from . import menu_handlers
-from . import docs_handlers
+# from . import docs_handlers
 from . import info
 from . import send_photos
 from . import location_handler
